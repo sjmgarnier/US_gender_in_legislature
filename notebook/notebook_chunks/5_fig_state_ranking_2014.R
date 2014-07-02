60a0a01a67aa743153a06ee7cd2cdc53974bd909
Simon Garnier <simon.garnier@gmail.com>
2014-07-03 09:39:30 +1000

    Changed color of bar chart to uniform.

--- notebook/notebook_chunks/5_fig_state_ranking_2014.R
+++ notebook/notebook_chunks/5_fig_state_ranking_2014.R
@@ -7,11 +7,10 @@ avg.house <- 100 * sum(tmp$HOUSE.WOMEN, na.rm = TRUE) / sum(tmp$HOUSE.TOTAL, na.
 # Senate + house combined
 g1 <- ggplot(data = tmp,
              aes(x = reorder(STATE.NAME, PERCENT.WOMEN), 
-                 y = PERCENT.WOMEN,
-                 fill = PERCENT.WOMEN)) +
+                 y = PERCENT.WOMEN)) +
   geom_hline(yintercept = avg.all, linetype = 3, size = 1) +
   geom_hline(yintercept = 50, linetype = 2, size = 1) +
-  geom_bar(stat = "identity", color = "white") +
+  geom_bar(stat = "identity", color = "white", fill = "dodgerblue4") +
   annotate("text", x = 1.25, y = avg.all + 2, label = "US average", angle = -90, 
            family = "Droid Sans Mono", hjust = 1, size = 5) +
   annotate("text", x = 1.25, y = 52, label = "Parity", angle = -90, 
@@ -22,8 +21,7 @@ g1 <- ggplot(data = tmp,
   ggtitle(bquote(atop("Gender ratio in state legislature", 
                       atop("Senate + House, by US state - 2014")))) +
   theme_graphzoo(base_size = 13, family = "Droid Sans Mono") + 
-  theme(axis.title.y = element_blank()) + 
-  scale_fill_continuous(low = "red4", high = "dodgerblue3", trans="log10")
+  theme(axis.title.y = element_blank())
 
 g1 <- addBanner(g1, font.size = 4, heights = c(1, 0.05*7/10),
                 l.txt = "GRAPHZOO.TUMBLR.COM", 
@@ -34,11 +32,10 @@ print(g1)
 # Senate alone
 g2 <- ggplot(data = tmp,
              aes(x = reorder(STATE.NAME, 100 * SENATE.WOMEN / SENATE.TOTAL), 
-                 y = 100 * SENATE.WOMEN / SENATE.TOTAL,
-                 fill = 100 * SENATE.WOMEN / SENATE.TOTAL)) +
+                 y = 100 * SENATE.WOMEN / SENATE.TOTAL)) +
   geom_hline(yintercept = avg.senate, linetype = 3, size = 1) +
   geom_hline(yintercept = 50, linetype = 2, size = 1) +
-  geom_bar(stat = "identity", color = "white") +
+  geom_bar(stat = "identity", color = "white", fill = "dodgerblue4") +
   annotate("text", x = 1.25, y = avg.senate + 2, label = "US average", angle = -90, 
            family = "Droid Sans Mono", hjust = 1, size = 5) +
   annotate("text", x = 1.25, y = 52, label = "Parity", angle = -90, 
@@ -49,8 +46,7 @@ g2 <- ggplot(data = tmp,
   ggtitle(bquote(atop("Gender ratio in state senates", 
                       atop("By US state - 2014")))) +
   theme_graphzoo(base_size = 13, family = "Droid Sans Mono") + 
-  theme(axis.title.y = element_blank()) + 
-  scale_fill_continuous(low = "red4", high = "dodgerblue3", trans="log10")
+  theme(axis.title.y = element_blank())
 
 g2 <- addBanner(g2, font.size = 4, heights = c(1, 0.05*7/10),
                 l.txt = "GRAPHZOO.TUMBLR.COM", 
@@ -62,11 +58,10 @@ print(g2)
 # representatives)
 g3 <- ggplot(data = filter(tmp, STATE.NAME != "Nebraska"),
              aes(x = reorder(STATE.NAME, 100 * HOUSE.WOMEN / HOUSE.TOTAL), 
-                 y = 100 * HOUSE.WOMEN / HOUSE.TOTAL,
-                 fill = 100 * HOUSE.WOMEN / HOUSE.TOTAL)) +
+                 y = 100 * HOUSE.WOMEN / HOUSE.TOTAL)) +
   geom_hline(yintercept = avg.house, linetype = 3, size = 1) +
   geom_hline(yintercept = 50, linetype = 2, size = 1) +
-  geom_bar(stat = "identity", color = "white") +
+  geom_bar(stat = "identity", color = "white", fill = "dodgerblue4") +
   annotate("text", x = 1.25, y = avg.house + 2, label = "US average", angle = -90, 
            family = "Droid Sans Mono", hjust = 1, size = 5) +
   annotate("text", x = 1.25, y = 52, label = "Parity", angle = -90, 
@@ -77,8 +72,7 @@ g3 <- ggplot(data = filter(tmp, STATE.NAME != "Nebraska"),
   ggtitle(bquote(atop("Gender ratio in state houses", 
                       atop("By US state - 2014")))) +
   theme_graphzoo(base_size = 13, family = "Droid Sans Mono") + 
-  theme(axis.title.y = element_blank()) + 
-  scale_fill_continuous(low = "red4", high = "dodgerblue3", trans="log10")
+  theme(axis.title.y = element_blank())
 
 g3 <- addBanner(g3, font.size = 4, heights = c(1, 0.05*7/10),
                 l.txt = "GRAPHZOO.TUMBLR.COM", 
